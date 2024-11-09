cb717bda069fc6ba769cffcf656bed2075e81b52
Jun-robot <saijun721.1@gmail.com>
2024-11-09 16:17:38 +0900

    教科書通り 13390

--- mul_div.c
+++ mul_div.c
@@ -14,7 +14,15 @@ static __inline int mul(int x,int y) {
 //
 // 以下を削除してここに乗算のロジックを入れてください。x=32bit , y=32bit, result=64bit
 //
-	return x*y;
+	uint64_t result=0;
+	for(int i=0; i<32; i++){
+		if(y&0x1){
+			result += x;
+		}
+		x <<= 1;
+		y >>= 1;
+	}
+	return result;
 }
 
 static __inline int div(int x,int y) {
@@ -39,7 +47,7 @@ int main() {
 
 
 	printf("Start (%s program)\n", LOGIN);
-
+	printf("LOOP_SEC = %d \n",LOOP_SEC);
 
 	// Benchmark MUL
 	count = 0;
